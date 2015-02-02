bb8d046c8aa5aa81fe458043ee3c2633030038c8
Shreyas <skraman1999@gmail.com>
2015-02-01 23:23:23 -0800

    Download a file.

--- anime-downloader-web.py
+++ anime-downloader-web.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, make_response
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+	response = make_response("hehehehehhehe")
+	response.headers["Content-Disposition"] = "attachment; filename=test.txt"
+	return response
 
 if __name__ == "__main__":
     app.run()
